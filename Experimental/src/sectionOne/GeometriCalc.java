1e56480683c59c97ff7c49fbf131efef3d7f1ff1
James Daniel <mathexmusic@gmail.com>
2013-10-04 14:06:22 -0400

    GeometricCalc- Working copy
    Attempt to commit 2x

--- Experimental/src/sectionOne/GeometriCalc.java
+++ Experimental/src/sectionOne/GeometriCalc.java
@@ -1,12 +1,13 @@
 package sectionOne;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class GeometriCalc {
 
 	public static void main(String[] args) {
+		
 		//initialization
-    	
     	System.out.println("This program was written by James Daniel");
     	System.out.println();
     	String varOne;
@@ -15,13 +16,69 @@ public class GeometriCalc {
         varOne = inlet.nextLine();
         System.out.println();
         
-        //do stuff
+        //declare vars
+        double width, height, base, radius;
+        String transChar;
         
+        System.out.print("Please select a shape abstratus. | [r]ectangle | [t]riangle | [c]ircle | >");
+        transChar = inlet.nextLine(); //transChar means transition to char
+        char caseSelector = transChar.charAt(0);
+        
+        switch(caseSelector)
+        {
+        	case 'r':
+        	case 'R':
+        		System.out.println("Rectangle chosen.");
+        		System.out.print("Please enter width: ");
+        		width = inlet.nextDouble();
+        		System.out.print("Please enter height: ");
+        		height = inlet.nextDouble();
+        		double rect = calcRectangle(width, height);
+        		System.out.println("The area of a rectangle with width " + width + " and height " + height + " is " + rect);
+        		break;
+        		
+        	case 't':
+        	case 'T':
+        		System.out.println("Triangle chosen.");
+        		System.out.print("Please enter base: ");
+        		base = inlet.nextDouble();
+        		System.out.print("Please enter height: ");
+        		height = inlet.nextDouble();
+        		double trig = calcRectangle(base, height);
+        		System.out.println("The area of a rectangle with base " + base + " and height " + height + " is " + trig);
+        		break;
+        		
+        	case 'c':
+        	case 'C':
+        		System.out.println("Circle chosen.");
+        		System.out.print("Please enter radius: ");
+        		radius = inlet.nextDouble();
+        		double circ = calcCircle(radius);
+        		System.out.println("The area of a circle with radius " + radius + " is " + circ);
+        		break;
+        }
+        
+        System.out.println();
         System.out.println("# # # # # # # # # # # # # # # # # # # #");
     	System.out.println("This program was run by " + varOne);
     	System.out.println("# # # # # # # # # # # # # # # # # # # #");
         inlet.close();
 
+	}
+	
+	public static double calcRectangle (double w, double h) {
+		double foo = w * h;
+		return foo;
+	}
+	
+	public static double calcTriangle (double b, double h) {
+		double bar = (0.5*b) * h;
+		return bar;
+	}
+	
+	public static double calcCircle (double r) {
+		double baz = r * (2*Math.PI);
+		return baz;
 	}
 
 }
