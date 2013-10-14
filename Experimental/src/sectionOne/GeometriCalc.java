c68f116eb33fac43ce3e1f5fbb3d2873dbc7bdf2
James Daniel <mathexmusic@gmail.com>
2013-10-14 18:42:06 -0400

    Closes #4; implements loop 6x

--- Experimental/src/sectionOne/GeometriCalc.java
+++ Experimental/src/sectionOne/GeometriCalc.java
@@ -17,67 +17,70 @@ public class GeometriCalc {
         varOne = inlet.nextLine();
         System.out.println();
         
-        //declare vars
-        double width, height, radius, side1, side2, side3;
-        String transChar;
-        
-        System.out.print("Please select a shape abstratus. | [r]ectangle | [t]riangle | [c]ircle | > ");
-        transChar = inlet.nextLine(); //transChar means transition to char
-        char caseSelector = transChar.charAt(0);
-        
-        // <switch case>
-        switch(caseSelector)
+        for (int i = 0; i < 6; ++i)
         {
-        	case 'r':
-        	case 'R':
-        		System.out.println("Rectangle chosen.");
-        		System.out.print("Please enter width: ");
-        		width = inlet.nextDouble();
-        		System.out.print("Please enter height: ");
-        		height = inlet.nextDouble();
-        		double rect = calcRectangle(width, height);
-        		System.out.println("The area of a rectangle with width " + width + " and height " + height + " is " + float2(rect));
-        		System.out.println("The perimeter of the same rectangle is " + float2((2*width + 2*height)));
-        		break;
-        		
-        	case 't':
-        	case 'T':
-        		System.out.println("Triangle chosen.");
-        		System.out.print("Please enter side 1: ");
-        		side1 = inlet.nextDouble();
-        		System.out.print("Please enter side 2: ");
-        		side2 = inlet.nextDouble();
-        		System.out.print("Please enter side 3: ");
-        		side3 = inlet.nextDouble();
-        		
-        		boolean checkIfValidTriangle = checkIfTriangle(side1, side2, side3);
-        		
-        		if (!checkIfValidTriangle)
-        			System.out.println("Invalid triangle.");
-        		else 
-        		{
-        			double trig = calcTriangle(side1, side2, side3);
-        			System.out.println("The area of a triangle with sides " + side1 + ", " + side2 + ", and " + side3 + " is " + float2(trig));
-        			System.out.println("The perimeter of the same triangle is " + float2((side1 + side2 + side3)));
-        		}
-        		break;
-        		
-        	case 'c':
-        	case 'C':
-        		System.out.println("Circle chosen.");
-        		System.out.print("Please enter radius: ");
-        		radius = inlet.nextDouble();
-        		double circ = calcCircle(radius);
-        		System.out.println("The area of a circle with radius " + radius + " is " + float2(circ));
-        		System.out.println("The circumference of the same circle is " + float2(radius * (2*Math.PI)));
-        		break;
-    		default:
-    			System.out.println("Invalid request.");
-    			break;
+        	//declare vars
+            double width, height, radius, side1, side2, side3;
+            String transChar;
+            
+            System.out.print("Please select a shape abstratus. | [r]ectangle | [t]riangle | [c]ircle | > ");
+            transChar = inlet.next(); //transChar means transition to char
+            char caseSelector = transChar.charAt(0);
+            
+            // <switch case>
+            switch(caseSelector)
+            {
+            	case 'r':
+            	case 'R':
+            		System.out.println("Rectangle chosen.");
+            		System.out.print("Please enter width: ");
+            		width = inlet.nextDouble();
+            		System.out.print("Please enter height: ");
+            		height = inlet.nextDouble();
+            		double rect = calcRectangle(width, height);
+            		System.out.println("The area of a rectangle with width " + width + " and height " + height + " is " + float2(rect));
+            		System.out.println("The perimeter of the same rectangle is " + float2((2*width + 2*height)));
+            		break;
+            		
+            	case 't':
+            	case 'T':
+            		System.out.println("Triangle chosen.");
+            		System.out.print("Please enter side 1: ");
+            		side1 = inlet.nextDouble();
+            		System.out.print("Please enter side 2: ");
+            		side2 = inlet.nextDouble();
+            		System.out.print("Please enter side 3: ");
+            		side3 = inlet.nextDouble();
+            		
+            		boolean checkIfValidTriangle = checkIfTriangle(side1, side2, side3);
+            		
+            		if (!checkIfValidTriangle)
+            			System.out.println("Invalid triangle.");
+            		else 
+            		{
+            			double trig = calcTriangle(side1, side2, side3);
+            			System.out.println("The area of a triangle with sides " + side1 + ", " + side2 + ", and " + side3 + " is " + float2(trig));
+            			System.out.println("The perimeter of the same triangle is " + float2((side1 + side2 + side3)));
+            		}
+            		break;
+            		
+            	case 'c':
+            	case 'C':
+            		System.out.println("Circle chosen.");
+            		System.out.print("Please enter radius: ");
+            		radius = inlet.nextDouble();
+            		double circ = calcCircle(radius);
+            		System.out.println("The area of a circle with radius " + radius + " is " + float2(circ));
+            		System.out.println("The circumference of the same circle is " + float2(radius * (2*Math.PI)));
+            		break;
+        		default:
+        			System.out.println("Invalid request.");
+        			break;
+            }
+            
+            System.out.println();
+            // </switch>
         }
-        
-        System.out.println();
-        // </switch>
         
         
         System.out.println();
