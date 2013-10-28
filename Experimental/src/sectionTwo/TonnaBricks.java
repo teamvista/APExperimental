ba42ffd261c311c1bd57ff517eda895934b66e38
James Daniel <mathexmusic@gmail.com>
2013-10-28 14:02:09 -0400

    Add shell of TonnaBricks.java

---
+++ Experimental/src/sectionTwo/TonnaBricks.java
@@ -0,0 +1,19 @@
+package sectionTwo;
+
+import java.applet.*;
+import java.awt.*;
+
+public class TonnaBricks extends Applet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public void init(){
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+
+
+}
