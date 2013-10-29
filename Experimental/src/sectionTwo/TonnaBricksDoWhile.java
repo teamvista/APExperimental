cf4d327fab9a95037e4d8253b5964cfb72ad4976
James Daniel <mathexmusic@gmail.com>
2013-10-29 13:29:36 -0400

    Add central structures for TonnaBricks files

---
+++ Experimental/src/sectionTwo/TonnaBricksDoWhile.java
@@ -0,0 +1,25 @@
+package sectionTwo;
+
+import java.applet.*;
+import java.awt.*;
+
+public class TonnaBricksDoWhile extends Applet {
+
+	private static final long serialVersionUID = 1L;
+	
+	//I DECLARE!!
+	public static final int DISPLAY_WIDTH = 600;
+	public static final int DISPLAY_HEIGHT = 600;
+	
+	public void init(){
+		resize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+		
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+
+
+}
