2c8f4dea2e669b53d5ffa976b88dcf3d4d5b3265
James Daniel <mathexmusic@gmail.com>
2013-11-07 20:02:41 -0500

    (For) code formatting

--- Experimental/src/sectionTwo/TonnaBricksFor.java
+++ Experimental/src/sectionTwo/TonnaBricksFor.java
@@ -6,8 +6,8 @@ import java.awt.*;
 public class TonnaBricksFor extends Applet {
 
 	private static final long serialVersionUID = 1L;
-	
-	//I DECLARE!!
+
+	// I DECLARE!!
 	public static final int DISPLAY_WIDTH = 600;
 	public static final int DISPLAY_HEIGHT = 600;
 	public int brickHeight = 50;
@@ -15,38 +15,37 @@ public class TonnaBricksFor extends Applet {
 	public int originX = 0;
 	public int originY = 0;
 	public int brickGap = 5;
-	
-	public void init(){
+
+	public void init() {
 		resize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-		Color midnightBlue = new Color(0,0,178);
+		Color midnightBlue = new Color(0, 0, 178);
 		setBackground(midnightBlue);
 	}
 
-	public void paint(Graphics g){
+	public void paint(Graphics g) {
 		int currentX = 0;
 		int currentY = 0;
-		
+
 		g.setColor(Color.darkGray);
-		
+
 		for (int brickRow = 1; brickRow <= DISPLAY_WIDTH; ++brickRow)
 		{
-			
+
 			for (int brickColumn = 1; brickColumn <= DISPLAY_HEIGHT; ++brickColumn)
 			{
 				g.fillRect(currentX, currentY, brickWidth, brickHeight);
 				currentX += (brickWidth + brickGap);
 			}
-			
+
 			if (brickRow % 2 == 0)
 				currentX = originX;
 			else
-				currentX = originX - brickWidth/2 - brickGap/2;
-			
+				currentX = originX - brickWidth / 2 - brickGap / 2;
+
 			currentY += brickHeight + brickGap;
-			
+
 		}
-		
-		
+
 	}
 
 
